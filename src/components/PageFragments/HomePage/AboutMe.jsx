6e3d77faba733feadcfe11f4c139a23e68e90018
Ahmed Komsan <ahmed.komsan2012@yahoo.com>
2021-06-06 01:13:34 +0200

    feat : updates

--- src/components/PageFragments/HomePage/AboutMe.jsx
+++ src/components/PageFragments/HomePage/AboutMe.jsx
@@ -6,7 +6,7 @@ import { stripTags, domHtml } from '../../../utils/stripTags';
 import SEO from '../../Seo';
 
 const pageText = {
-  paraOne: `Hello !! My name is Rolwin Reevan Monteiro. I'm a full stack web developer who is
+  paraOne: `Hello !! My name is Ahmed Komsan Monteiro. I'm a full stack web developer who is
     passionate about various web technologies. I like to experiment with different web
     technologies. I have an experience of nearly 3 years working with LAMP stack, MERN stack
     and ELK stack. Building fancy UI's just like this one that your seeing 😅 and writing blogs about tech stacks
--- src/components/PageLayout/Sidebar/index.jsx
+++ src/components/PageLayout/Sidebar/index.jsx
@@ -21,12 +21,12 @@ const DomContent = () => (
     <div className={`${style.name} centerAlign`}>
       <div className={`${style.boxName} centerAlign`}>
         <h2>
-          Rolwin
+          Ahmed
           {' '}
-          <span>Reevan</span>
+          <span>Komsan</span>
         </h2>
       </div>
-      <div className={`${style.badge} ${style.badgeGray}`}>Software Engineer</div>
+      <div className={`${style.badge} ${style.badgeGray}`}>IOS Software Engineer</div>
       <div className="centerAlign box">
         <a href={facebook} target="_blank" label="button" rel="noopener noreferrer"><FA name="facebook-f" /></a>
         <a href={twitter} target="_blank" label="button" rel="noopener noreferrer"><FA name="twitter" /></a>
@@ -39,19 +39,19 @@ const DomContent = () => (
             <FeatherIcon size="19" icon="calendar" />
             {' '}
           </span>
-&nbsp; &nbsp; May 9,1995
+&nbsp; &nbsp; June 22,1993
         </li>
         <li className={`${style.contactBlockItem}`}>
           <span><FeatherIcon size="19" icon="map-pin" /></span>
           {' '}
-&nbsp; &nbsp; Bangalore, India
+&nbsp; &nbsp; Cairo, Egypt
         </li>
         <li className={`${style.contactBlockItem}`}>
           <span><FeatherIcon size="19" icon="mail" /></span>
           {' '}
 &nbsp; &nbsp;
           <a
-            href="mailto:&#114;&#111;&#108;&#119;&#105;&#110;&#109;&#111;&#110;&#116;&#101;&#105;&#114;&#111;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;"
+            href="mailto:ahmedkomsan0@gmail.com"
             target="_top"
           >
             <span className={style.emailHider}>@</span>
