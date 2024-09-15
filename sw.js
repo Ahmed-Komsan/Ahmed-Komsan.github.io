f71bfebdde7fa3730133868d44da6c456370d47c
Ahmed Komsan <ahmed.komsan2012@yahoo.com>
2024-09-15 18:53:37 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-09617ab1a7ff05df783e.js"
+    "url": "webpack-runtime-b1c710912b5d60653b76.js"
   },
   {
     "url": "framework-122a973a865ebc35004a.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "77ba3be5b7b3957e10bed6112bbad878"
+    "revision": "1d0c617a9810c3a2f74603612bf87b7a"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-0ff8482a9b4b7c052472.js"
